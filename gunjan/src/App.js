5af34ad8821d65c1ae5cb9d5d317c7684d07314c
Gunjan <gpt47knl2@gmail.com>
2020-07-30 21:39:49 +0530

    Gettin Started

---
+++ gunjan/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './Header';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets Build Tinder Clone</h1>
+      {/* Header */}
+      <Header/>
+      {/* Tinder Cards */}
+      {/* Buttons Below Tinder Cards*/}
+
+      {/* chat Screen*/}
+      {/* Indivisual Chat Screen*/}
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ gunjan/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PersonIcon from '@material-ui/icons/Person';
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+
+function Header() {
+    return(
+        <div>
+            <PersonIcon/>
+            <h2> I am a Header</h2>
+        </div>
+
+    );
+}
+export default Header;
+
